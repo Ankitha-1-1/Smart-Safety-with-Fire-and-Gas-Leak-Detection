5c48adb3a5dc30dfa7b2a9ec6433d690f494521c
Ankitha-1-1 <ankithapramod11@gmail.com>
2026-08-06 20:42:25 +0530

    Update Config.h

---
+++ arduino/Config.h
@@ -0,0 +1,17 @@
+#define LM35_PIN A0
+#define MQ2_PIN A1
+
+#define LCD_RS 12
+#define LCD_EN 11
+#define LCD_D4 5
+#define LCD_D5 4
+#define LCD_D6 3
+#define LCD_D7 2
+
+#define GSM_RX 8
+#define GSM_TX 9
+
+#define BUZZER_PIN 10
+
+#define GAS_THRESHOLD 400
+#define FIRE_THRESHOLD 45
