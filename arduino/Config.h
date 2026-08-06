5659501557e61a5651c31a2c2a55e37dfd2d44a0
Ankitha-1-1 <ankithapramod11@gmail.com>
2026-08-06 20:58:09 +0530

    Update Config.h

--- arduino/Config.h
+++ arduino/Config.h
@@ -1,6 +1,10 @@
-#define LM35_PIN A0
-#define MQ2_PIN A1
+#ifndef CONFIG_H
+#define CONFIG_H
 
+#include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
+
+// LCD
 #define LCD_RS 12
 #define LCD_EN 11
 #define LCD_D4 5
@@ -8,10 +12,20 @@
 #define LCD_D6 3
 #define LCD_D7 2
 
+// Sensors
+#define LM35_PIN A0
+#define MQ2_PIN  A1
+
+// Output
+#define BUZZER_PIN 10
+
+// GSM
 #define GSM_RX 8
 #define GSM_TX 9
 
-#define BUZZER_PIN 10
+const int GAS_THRESHOLD = 400;
+const float FIRE_THRESHOLD = 45.0;
 
-#define GAS_THRESHOLD 400
-#define FIRE_THRESHOLD 45
+const char PHONE[] = "+91XXXXXXXXXX";
+
+#endif
