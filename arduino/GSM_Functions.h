31acb521be0489386c4791661baa2e797be7aab0
Ankitha-1-1 <ankithapramod11@gmail.com>
2026-08-06 20:44:23 +0530

    Update GSM_Functions.h

---
+++ arduino/GSM_Functions.h
@@ -0,0 +1,39 @@
+#ifndef GSM_FUNCTIONS_H
+#define GSM_FUNCTIONS_H
+
+#include <SoftwareSerial.h>
+
+extern SoftwareSerial mySerial;
+
+// Change this to your phone number
+const String PHONE_NUMBER = "+91XXXXXXXXXX";
+
+void updateSerial()
+{
+  delay(500);
+
+  while (mySerial.available())
+  {
+    Serial.write(mySerial.read());
+  }
+}
+
+void sendSMS(String message)
+{
+  mySerial.println("AT+CMGF=1");      // Set SMS mode
+  updateSerial();
+
+  mySerial.print("AT+CMGS=\"");
+  mySerial.print(PHONE_NUMBER);
+  mySerial.println("\"");
+  updateSerial();
+
+  mySerial.print(message);
+
+  delay(500);
+
+  mySerial.write(26);   // Ctrl+Z to send SMS
+  updateSerial();
+}
+
+#endif
